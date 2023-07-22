09098a690cf3f4d8421a726ba16e6cf113eb7a8d
Mythical-Github <MythicalData@gmail.com>
2023-07-22 00:03:00 -0400

    Edited Example C++ Mod Code
    Edited Example C++ Mod Code

--- C++/MyMods/ExampleMod/dllmain.cpp
+++ C++/MyMods/ExampleMod/dllmain.cpp
@@ -1,4 +1,4 @@
-#include ModCppUserModBase.hpp
+#include <Mod/CppUserModBase.hpp>
 #include <DynamicOutput/DynamicOutput.hpp>
 #include <Unreal/UObjectGlobals.hpp>
 #include <Unreal/UObject.hpp>
@@ -6,47 +6,47 @@
 using namespace RC;
 using namespace RC::Unreal;
 
-class ExampleMod  public RCCppUserModBase
+class ExampleMod : public RC::CppUserModBase
 {
-public
-    ExampleMod()  CppUserModBase()
+public:
+    ExampleMod() : CppUserModBase()
     {
-        ModName = STR(ExampleMod);
-        ModVersion = STR(1.0);
-        ModDescription = STR(This is my awesome mod);
-        ModAuthors = STR(UE4SS Team);
-         Do not change this unless you want to target a UE4SS version
-         other than the one you're currently building with somehow.
-        ModIntendedSDKVersion = STR(2.6);
+        ModName = STR("ExampleMod");
+        ModVersion = STR("1.0");
+        ModDescription = STR("This is an example C++ mod");
+        ModAuthors = STR("UE4SS Team");
+        // Do not change this unless you want to target a UE4SS version
+        // other than the one you're currently building with somehow.
+        //ModIntendedSDKVersion = STR("2.6");
         
-        Output::send<LogLevel::Verbose>(STR("MyAwesomeMod says hello\n"));
+        Output::send<LogLevel::Verbose>(STR("ExampleMod says hello\n"));
     }
-
+	
     ~ExampleMod()
     {
     }
 	
 	auto on_unreal_init() -> void override
-{
-    // You are allowed to use the 'Unreal' namespace in this function and anywhere else after this function has fired.
-    auto Object = UObjectGlobals::StaticFindObject(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
-    Output::send<LogLevel::Verbose>(STR("Object Name: {}\n"), Object->GetFullName());
-}
+	{
+		// You are allowed to use the 'Unreal' namespace in this function and anywhere else after this function has fired.
+		auto Object = UObjectGlobals::StaticFindObject(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
+		Output::send<LogLevel::Verbose>(STR("Object Name: {}\n"), Object->GetFullName());
+	}
 	
-    auto on_update() - void override
+    auto on_update() -> void override
     {
     }
 };
 
 #define EXAMPLE_MOD_API __declspec(dllexport)
-extern C
+extern "C"
 {
-    EXAMPLE_MOD_API RCCppUserModBase start_mod()
+    EXAMPLE_MOD_API RC::CppUserModBase* start_mod()
     {
         return new ExampleMod();
     }
 
-    EXAMPLE_MOD_API void uninstall_mod(RCCppUserModBase mod)
+    EXAMPLE_MOD_API void uninstall_mod(RC::CppUserModBase* mod)
     {
         delete mod;
     }
